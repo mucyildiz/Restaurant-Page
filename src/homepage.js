5d2856884cef48060fc152442f16e02ab5b7692e
Alperen Yildiz <alperenyildiz21@gmail.com>
2020-06-16 16:54:02 -0400

    changed homepage content styling

--- src/homepage.js
+++ src/homepage.js
@@ -47,13 +47,22 @@ const createHomePage = (function() {
         let pageContent = document.querySelector('.pageContent');
 
         let homePageContent = document.createElement('div');
-        homePageContent.innerHTML = '-Fake Restaurant-';
+        homePageContent.innerHTML = 'Fake Restaurant';
         homePageContent.className = 'homepage-content';
 
         let homePageContentSubtitle = document.createElement('div');
         homePageContentSubtitle.innerHTML = "It's simply not real - Guy Fieri";
         homePageContentSubtitle.className = 'home-sub';
 
+        let menuButtonArea = document.createElement('div');
+
+        let menuButton = document.createElement('button');
+        menuButton.innerHTML = 'See Menu';
+        menuButton.className = 'menu-button';
+
+
+        menuButtonArea.appendChild(menuButton);
+        homePageContentSubtitle.appendChild(menuButtonArea);
         homePageContent.appendChild(homePageContentSubtitle);
         pageContent.appendChild(homePageContent);
     }
